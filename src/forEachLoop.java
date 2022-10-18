9d3b467210a51a3d6d2bccc16ff76d44f8fc0129
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2022-10-18 19:48:40 -0400

    added urls

--- src/forEachLoop.java
+++ src/forEachLoop.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class forEachLoop {
+import java.util.ArrayList;
+
+public class forEachLoop {
+
+    //https://www.youtube.com/watch?v=t8mmNbgKA8w
+
+    public static void main(String[] args) {
+        /*String [] cars = {"escape", "fusion", "explorer"};
+        for (int i = 0; i < cars.length; i++){                  //for loop
+            System.out.println(cars[i]);
+        }
+        for (String car : cars){                                //for each loop
+            System.out.println(car);
+        }*/
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(3);
+        numbers.add(7);
+        numbers.add(2);
+
+        for (int a : numbers){
+            System.out.println(a);
+        }
+        for (int i = 0; i< numbers.size(); i++){
+            System.out.println(numbers.get(i));
+        }
+    }
 }
--- src/nestedForLoop.java
+++ src/nestedForLoop.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class nestedForLoop {
+public class nestedForLoop {
+
+    public static void main(String[] args) {
+
+        //https://www.youtube.com/watch?v=sk9xYje9XAQ&list=PL59LTecnGM1Mg6I4i_KbS0w5bPcDjl7oz&index=1
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("this will print 5 times");
+        }
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        String[] colors = {"red", "blue", "green"};
+        for (int i = 0; i < colors.length; i++){
+            System.out.println(colors[i]);
+        }
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        String[][] fancyColors = {{"red", "blue", "green"},
+                                  {"cyan", "magenta", "turquoise"}};
+        for (int i = 0; i < 2; i++){                                            //loops through row
+            for (int j = 0; j < 3; j++){                                        //loops through column
+                System.out.println(fancyColors[i][j]);
+            }
+        }
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 10; j++){
+                System.out.println("i is "+ i +" and j is "+ j);
+            }
+        }
+    }
+
 }
